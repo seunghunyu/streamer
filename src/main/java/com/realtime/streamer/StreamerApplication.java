41bb23a6e81f0e2c31930d16153f792c4c8e5ece
DESKTOP-F12VINP\User <yush@obzen.com>
2023-06-01 16:32:16 +0900

    토픽생성, jdbc 관련 설정 추가

--- src/main/java/com/realtime/streamer/StreamerApplication.java
+++ src/main/java/com/realtime/streamer/StreamerApplication.java
@@ -1,16 +1,19 @@
 package com.realtime.streamer;
 
 import com.realtime.streamer.topic.CreateTopic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class StreamerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamerApplication.class, args);
-		CreateTopic createTopic = new CreateTopic();
-		createTopic.create();
 	}
 
 }
--- src/main/java/com/realtime/streamer/topic/CreateTopic.java
+++ src/main/java/com/realtime/streamer/topic/CreateTopic.java
@@ -1,5 +1,8 @@
 package com.realtime.streamer.topic;
 
+import com.realtime.streamer.execution.GetReadyCamp;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.ClientRequest;
 import org.apache.kafka.clients.ClientResponse;
 import org.apache.kafka.clients.KafkaClient;
@@ -10,13 +13,23 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.errors.AuthenticationException;
 import org.apache.kafka.common.requests.AbstractRequest;
+import org.rocksdb.util.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.*;
-
-public class CreateTopic {
+@Slf4j
+@RequiredArgsConstructor
+@Component //Main 호출시 스캔
+//@Order(1) -> ApplicationRunner 순서 정해 줄 수 있음
+//public class CreateTopic implements ApplicationRunner {
+public class CreateTopic{
     Properties properties;
     Admin admin;
 
@@ -24,9 +37,15 @@ public class CreateTopic {
     @Value("${kafka.bootstrapAddress}")
     private String bootstrapServers;
 
-    public CreateTopic() {
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        System.out.println("Application Runner@@@@@@@");
+//    }
+
+    @Bean
+    public void create() {
+        System.out.println("create1 ");
         //프로퍼티 생성
-        System.out.println(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG);
         System.out.println(bootstrapServers);
         Map<String,Object> map = new HashMap<>();
         properties = new Properties();
@@ -34,16 +53,32 @@ public class CreateTopic {
         //kafka.clients.admin 객체 생성
         admin = Admin.create(properties);
 
-    }
-
-    public void create(){
         //String 토픽명, int 파티션, (short) replicationFactor
-        NewTopic newTopic = new NewTopic("topic1", 3, (short)3);
+        NewTopic newTopic = new NewTopic("topic1", 1, (short)1);
         try{
             admin.createTopics(Collections.singleton(newTopic)).all().get();
         }catch(Exception e) {
-            e.printStackTrace();
+            // e.printStackTrace();
         }
+
     }
 
+    public void create2(){
+        System.out.println("create2 ");
+        //String 토픽명, int 파티션, (short) replicationFactor
+        NewTopic newTopic = new NewTopic("topic1", 1, (short)1);
+        try{
+            admin.createTopics(Collections.singleton(newTopic)).all().get();
+        }catch(Exception e) {
+           // e.printStackTrace();
+        }
+    }
+    //수행 캠페인 정보 가져오기
+    @Bean
+    public void getRunningCamp(){
+        System.out.println("getRunningCamp!!!");
+        log.info("getRunningCamp!!!");
+        GetReadyCamp getReadyCamp = new GetReadyCamp();
+        getReadyCamp.polling();
+    }
 }
