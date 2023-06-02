c2eeace2ff7441975b41b82945df4a87997ae88e
DESKTOP-F12VINP\User <yush@obzen.com>
2023-06-02 09:42:21 +0900

    사용중 캠페인 감지채널 조회 쿼리 추가

--- src/main/java/com/realtime/streamer/execution/GetReadyCamp.java
+++ src/main/java/com/realtime/streamer/execution/GetReadyCamp.java
@@ -1,5 +1,26 @@
 package com.realtime.streamer.execution;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
 public class GetReadyCamp {
+
+    String getDetcChanQry = " SELECT B.DETC_CHAN_CD, COUNT(*) "
+                          + "   FROM ECUBEEBM.R_PLAN A, ECUBEEBM.R_DETC_CHAN B "
+                          + "  WHERE A.CAMP_ID = B.CAMP_ID "
+                          + "    AND A.CAMP_STAT in ('3100', '3000') "
+                          + "    AND A.CAMP_EX_TY = '9' "
+                          + "    AND A.CAMP_STR_DT <= ? "
+                          + "    AND A.CAMP_END_DT >= ? ";
+
+
+    public void polling(){
+        System.out.println("polling :: " );
+        log.debug("GetReadyCamp polling Interval : " );
+
+
+
+    }
 
 }
